74f5db697cdff5b46829b063eb86262451ddcd26
Caroline <caroline_mendez@baylor.edu>
2020-08-23 19:24:47 -0500

    Merge Sort

---
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -0,0 +1,84 @@
+import React from 'react';
+import * as sortingAlgorithms from '../sortingAlgorithms/sortingAlgorithms';
+import '../SortingVisualizer/SortingVisualizer.css';
+
+export default class SortingVisualizer extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            array: [],
+        };
+    }
+
+    componentDidMount() {
+        this.resetArray();
+    }
+
+    resetArray() {
+        const array = [];
+        for (let i = 0; i < 200; i++) {
+            array.push(randomIntFromInterval(5, 700));
+        }
+        this.setState({ array });
+    }
+
+    mergeSort() {
+        const javaScriptSortedArray = this.state.array.slice().sort((a, b) => a - b);
+        const sortedArray = sortingAlgorithms.mergeSort(this.state.array);
+
+        console.log(arraysAreEqual(javaScriptSortedArray, sortedArray));
+    }
+
+    quickSort() { }
+
+    heapSort() { }
+
+    bubbleSort() { }
+
+    testSortingAlgorithms() {
+        for (let i = 0; i < 100; i++) {
+            const array = [];
+            const bound = randomIntFromInterval(1, 1000)
+            for (let i = 0; i < bound; i++) {
+                array.push(randomIntFromInterval(-1000, 1000));
+            }
+            const javaScriptSortedArray = array.slice().sort((a, b) => a - b);
+            const mergeSortedArray = sortingAlgorithms.mergeSort(array.slice());
+            console.log(arraysAreEqual(javaScriptSortedArray, mergeSortedArray));
+        }
+    }
+
+    render() {
+        const { array } = this.state;
+
+        return (
+            <div className="array-container">
+                {
+                    array.map((value, idx) => (
+                        <div className="array-bar" key={idx} style={{ height: `${value}px` }}>
+                        </div>
+                    ))
+                }
+                <button onClick={() => this.resetArray()}>Generate New Array</button>
+                <button onClick={() => this.mergeSort()}>Merge Sort</button>
+                <button onClick={() => this.quickSort()}>Quick Sort</button>
+                <button onClick={() => this.heapSort()}>Heap Sort</button>
+                <button onClick={() => this.bubbleSort()}>Bubble Sort</button>
+                <button onClick={() => this.testSortingAlgorithms()}>Test Algorithms</button>
+            </div>
+        );
+    }
+}
+
+function randomIntFromInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function arraysAreEqual(arrayOne, arrayTwo) {
+    if (arrayOne.length !== arrayTwo.length) return false;
+    for (let i = 0; i < arrayOne.length; i++) {
+        if (arrayOne[i] !== arrayTwo[i]) return false;
+    }
+    return true;
+}
